f47c04d28cb765a3eda17acc23bb3f454bf1cb79
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2024-01-27 03:48:01 +0100

    added small changes to CardManager.cs

--- Assets/Scripts/CardBehaviour.cs
+++ Assets/Scripts/CardBehaviour.cs
@@ -4,35 +4,40 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CardBehaviour : MonoBehaviour
+namespace GGJ
 {
-    public enum difficulty { tutorial, veryEasy, easy, medium, hard, veryHard, main_story_1, main_story_2, main_story_3, main_story_4, main_story_5, main_story_6, main_story_7 };
-    public class Option
+
+    public class CardBehaviour : MonoBehaviour
     {
-        public string text;
-        public float audience, money, security;
-        public string achievement, extra;
-    }
+        public enum difficulty { tutorial, veryEasy, easy, medium, hard, veryHard, main_story_1, main_story_2, main_story_3, main_story_4, main_story_5, main_story_6, main_story_7 };
+        public class Option
+        {
+            public string text;
+            public float audience, money, security;
+            public string achievement, extra;
+        }
 
-    public int ID;
-    public bool audience;
-    public bool money;
-    public bool security;
-    public difficulty diff;
-    public Image character_art;
-    public string description;
-    public Option top;
-    public Option bottom;
+        public int ID;
+        public bool audience;
+        public bool money;
+        public bool security;
+        public difficulty diff;
+        public Image character_art;
+        public string description;
+        public Option top;
+        public Option bottom;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+        }
     }
 }
+    
--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -2,34 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
+using static GGJ.CardBehaviour;
 
-public class CardManager : MonoBehaviour
+namespace GGJ
 {
-    public TMP_Text daysCounter;
-
-    [Header("STATS")]
-    public Image audienceStat;
-    public Image moneyStat;
-    public Image securityStat;
-
-    [Header("EVENT")]
-    public Image eventImage;
-    public TMP_Text eventDescription;
-    public Button option1;
-    public Button option2;
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class CardManager : MonoBehaviour
     {
-        
+        public TMP_Text daysCounter;
+
+        [Header("STATS")]
+        public Image audienceStat;
+        public Image moneyStat;
+        public Image securityStat;
+
+        [Header("EVENT")]
+        public Image eventImage;
+        public TMP_Text eventDescription;
+        public Button option1;
+        public Button option2;
+
+        [Header("DATA")]
+        public Option selectedOption;
+
+        public static UnityEvent onOptionSelected = new UnityEvent();
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            onOptionSelected.AddListener(UpdateStats);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape)) 
+            {
+                Option a = new Option();
+                a.audience = 0.2f;
+                a.money = -0.5f;
+                a.security = 0f;
+
+                selectedOption = a;
+
+                onOptionSelected.Invoke();
+            }
+        }
+
+        void UpdateStats()
+        {
+            moneyStat.fillAmount += selectedOption.audience;
+            audienceStat.fillAmount += selectedOption.money;
+            securityStat.fillAmount += selectedOption.security;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
